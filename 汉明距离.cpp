0baa70e9c0c2c1136d1891b048af4ddd2fd0587f
changwenwu <wucw5513@mails.jlu.edu.cn>
2018-08-03 10:31:54 +0800

    汉明距离

--- 汉明距离.cpp
+++ 汉明距离.cpp
@@ -6,3 +6,18 @@
 //
 
 #include <stdio.h>
+
+//先做异或操作，再计算位1的个数
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int z = x ^ y;
+        
+        int result = 0;
+        while(z > 0){
+            result += z % 2;
+            z = z / 2;
+        }
+        return result;
+    }
+};
