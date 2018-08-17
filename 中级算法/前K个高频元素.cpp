9e7e9372cf7b0194e246ec1e967feed1d66f09d3
changwenwu <wucw5513@mails.jlu.edu.cn>
2018-08-17 17:29:48 +0800

    前K个高频元素

---
+++ 中级算法/前K个高频元素.cpp
@@ -0,0 +1,24 @@
+//
+//  前K个高频元素.cpp
+//  Leetcode
+//
+//  Created by jarvis on 2018/8/17.
+//
+
+#include <stdio.h>
+
+//优先队列
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> m;
+        priority_queue<pair<int, int>> q;
+        vector<int> res;
+        for (auto a : nums) ++m[a];
+        for (auto it : m) q.push({it.second, it.first});
+        for (int i = 0; i < k; ++i) {
+            res.push_back(q.top().second); q.pop();
+        }
+        return res;
+    }
+};
