0c35ce180fbf69cca3bd0ce5776011b8fe737edc
jarviswu <wucw5513@mails.jlu.edu.cn>
2018-08-28 23:06:09 +0800

    两数相除

---
+++ 中级算法/两数相除.cpp
@@ -0,0 +1,28 @@
+//
+//  两数相除.cpp
+//  RxSwiftPlayground
+//
+//  Created by Jarvis Wu on 2018/8/28.
+//  Copyright © 2018年 Scott Gardner. All rights reserved.
+//
+
+#include <stdio.h>
+//位运算
+class Solution {
+    public:
+    int divide(int dividend, int divisor) {
+        long long m = abs((long long)dividend), n = abs((long long)divisor), res = 0;
+        if (m < n) return 0;
+        while (m >= n) {
+            long long t = n, p = 1;
+            while (m > (t << 1)) {
+                t <<= 1;
+                p <<= 1;
+            }
+            res += p;
+            m -= t;
+        }
+        if ((dividend < 0) ^ (divisor < 0)) res = -res;
+        return res > INT_MAX ? INT_MAX : res;
+    }
+};
