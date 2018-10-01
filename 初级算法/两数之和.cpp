79f5ceb8266ee449545ecc6775a0bf4a4a6f902b
Jarvis Wu <wucw5513@mails.jlu.edu.cn>
2018-10-01 12:44:30 +0800

    两数之和（二刷优化算法）

--- 初级算法/两数之和.cpp
+++ 初级算法/两数之和.cpp
@@ -30,3 +30,24 @@ public:
         
     }
 };
+
+//二刷优化算法
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        unordered_map<int, int> m;
+        int len = nums.size();
+        for(int i = 0; i < len; i++){
+            if(m.find(target - nums[i]) != m.end()){
+                result.push_back(i);
+                result.push_back(m[target - nums[i]]);
+                return result;
+            }else{
+                m[nums[i]] = i;
+            }
+        }
+        
+        return result;
+    }
+};
