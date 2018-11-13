235c8b0c9449dc3e51ab137d56c428f5a899b2f0
changwenwu <wucw5513@mails.jlu.edu.cn>
2018-11-13 17:23:16 +0800

    Two Sum

---
+++ tessssst/tessssst/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  tessssst
+//
+//  Created by jarvis on 2018/11/13.
+//  Copyright © 2018 JarvisWu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    return 0;
+}
---
+++ 顺序刷题/1. Two Sum.cpp
@@ -0,0 +1,29 @@
+//
+//  1. Two Sum.cpp
+//  Leetcode
+//
+//  Created by jarvis on 2018/11/13.
+//
+
+#include <stdio.h>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        unordered_map<int, int> m;
+        for(int i = 0; i < nums.size(); i++){
+            if(m.find(nums[i]) != m.end()){
+                result.push_back(i);
+                result.push_back(m[nums[i]]);
+                break;
+            }
+            m[target - nums[i]] = i;
+        }
+        return result;
+    }
+};
