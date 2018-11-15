b9efc068133337b755187014a79cfab48ed9d1d3
changwenwu <wucw5513@mails.jlu.edu.cn>
2018-11-15 10:56:08 +0800

    code format

--- 顺序刷题/4. Median of two sorted arrays.cpp
+++ 顺序刷题/4. Median of two sorted arrays.cpp
@@ -1,9 +1,9 @@
 //
-//  4.median of two sorted arrays.cpp
-//  CleanArchitectureRxSwift
+//  4.Median of two sorted arrays.cpp
+//  Leetcode
 //
-//  Created by Jarvis Wu on 2018/11/14.
-//  Copyright © 2018年 sergdort. All rights reserved.
+//  Created by jarvis on 2018/11/13.
+//  Copyright © 2018 JarvisWu. All rights reserved.
 //
 
 #include <stdio.h>
@@ -11,21 +11,25 @@
 #include <unordered_map>
 
 using namespace std;
-// 递归+二分解法，先将问题分解为求联合数组中下标为(m + n + 1) / 2 与 (m + n + 2) / 2的平均值（对奇偶都适用），定义求两个数组中第K个数的函数
+// 递归+二分解法，先将问题分解为求联合数组中下标为(m + n + 1) / 2 与 (m + n + 2) /
+// 2的平均值（对奇偶都适用），定义求两个数组中第K个数的函数
 class Solution {
-public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = (int)nums1.size(), n = (int)nums2.size(), left = (m + n + 1) / 2, right = (m + n + 2) / 2;
+  public:
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+        int m = (int) nums1.size(), n = (int) nums2.size(), left = (m + n + 1) / 2, right = (m + n + 2) / 2;
         return (findKth(nums1, 0, nums2, 0, left) + findKth(nums1, 0, nums2, 0, right)) / 2.0;
     }
-    
-    double findKth(vector<int>& nums1, int i, vector<int>& nums2, int j, int k){
+
+    double findKth(vector<int> &nums1, int i, vector<int> &nums2, int j, int k) {
         // 起始值大于数组大小则代表该数组已经遍历完，返回另一个数组中第K个数即可
-        if (i >= nums1.size()) return nums2[j + k - 1];
-        if (j >= nums2.size()) return nums1[i + k - 1];
+        if (i >= nums1.size())
+            return nums2[j + k - 1];
+        if (j >= nums2.size())
+            return nums1[i + k - 1];
         // k为1时直接返回两数组首部较小值
-        if (k == 1) return min(nums1[i], nums2[j]);
-        
+        if (k == 1)
+            return min(nums1[i], nums2[j]);
+
         // 将k二分，超出数组范围则定义为无穷大
         double k1 = (k / 2 + i - 1) < nums1.size() ? nums1[k / 2 + i - 1] : INT_MAX;
         double k2 = (k / 2 + j - 1) < nums2.size() ? nums2[k / 2 + j - 1] : INT_MAX;
--- 顺序刷题/5.Longest palindromic substring.cpp
+++ 顺序刷题/5.Longest palindromic substring.cpp
@@ -1,9 +1,9 @@
 //
-//  5.longest palindromic substring.cpp
-//  CleanArchitectureRxSwift
+//  5.Longest palindromic substring.cpp
+//  Leetcode
 //
-//  Created by Jarvis Wu on 2018/11/14.
-//  Copyright © 2018年 sergdort. All rights reserved.
+//  Created by jarvis on 2018/11/13.
+//  Copyright © 2018 JarvisWu. All rights reserved.
 //
 
 #include <stdio.h>
@@ -14,22 +14,25 @@
 using namespace std;
 // 动态规划算法，从字母中心向两边扩散
 class Solution {
-public:
+  public:
     string longestPalindrome(string s) {
-        if(s.empty()) return "";
+        if (s.empty())
+            return "";
         int start = 0, maxLen = 1;
-        for(int i = 0; i < s.size();){
-            if(s.size() - i <= maxLen / 2) break; // 剪枝
+        for (int i = 0; i < s.size();) {
+            if (s.size() - i <= maxLen / 2)
+                break; // 剪枝
             int left = i, right = i;
             // 遇见相同元素时直接跳到最后一位
-            while(right < s.size() - 1 && s[right] == s[right + 1]) right++;
+            while (right < s.size() - 1 && s[right] == s[right + 1])
+                right++;
             i = right + 1;
             // 扩散
             while (left > 0 && right < s.size() - 1 && s[left - 1] == s[right + 1]) {
                 left--;
                 right++;
             }
-            if(maxLen < right - left + 1){
+            if (maxLen < right - left + 1) {
                 start = left;
                 maxLen = right - left + 1;
             }
